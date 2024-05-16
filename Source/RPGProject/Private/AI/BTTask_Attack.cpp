2b384f68e5863904e097ca9ffc3e517456ab944c
shoon0123 <shoon0123@gmail.com>
2024-05-16 22:44:29 +0900

    feat: Add BTTask_Attack

---
+++ Source/RPGProject/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTTask_Attack.h"
+
+UBTTask_Attack::UBTTask_Attack()
+{
+	NodeName = TEXT("Attack");
+}
